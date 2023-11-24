06c9ab38923e4fc77c03bd91bff863013c242ec0
stepanovj <evgenii-stepanov@bk.ru>
2023-11-24 19:49:48 +0300

    add tests on function

--- temp_grouper/main.cpp
+++ temp_grouper/main.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+bool tests(){
+vector<float> a{10.3, 10.7, -25.4, 15.5, 15.2, 36.1};
+    map<int, std::vector<float>> m = groupTemperatures(a);
+    for(const auto& [element, temps] : m){
+        if(element == 10 && temps.size() != 4) return false;
+        if(element == 30 && temps.size() != 1) return false;
+        return true;
+    }
+}
+
 int main()
 {
     vector<float> a{10.3, 10.7, -25.4, 15.5, 15.2, 36.1};
@@ -14,5 +24,9 @@ int main()
         for(int i = 0; i < temps.size(); i++) cout << temps[i] << " ";
         cout << endl;
     }
+
+    cout << "---------------------------" << endl;
+    if(tests() == true) cout << "tests pass";
+    else cout << "tests failed";
     return 0;
 }
